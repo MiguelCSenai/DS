8d8499a75722edf48bd1f14b2a39de1f1be0f601
Miguel <cairesmontalvaomiguel@gmail.com>
2024-04-08 11:18:18 -0300

    Segunda-Feira - 08/04/24

--- Java/IntroducaoJava/src/introducaojava/Exercicio9.java
+++ Java/IntroducaoJava/src/introducaojava/Exercicio9.java
@@ -7,8 +7,10 @@ public class Exercicio9 {
     public static void main(String[] args) {
         
         String sNome = "";
-        String sIdade = "";
+        int iIdade = 0;
         String sTexto = "Digite um nome (Mais de 3 letras)";
+        float fSal =0;
+        String sGen = "";
         do{
         
             sNome = JOptionPane.showInputDialog(sTexto);
@@ -21,11 +23,32 @@ public class Exercicio9 {
         
         do{
             
-            sNome = JOptionPane.showInputDialog(sTexto);
-            sTexto = "NOME INVALIDO!!!\n\nDigite uma idade (Entre 0 e 150)";
+            iIdade = Integer.parseInt(JOptionPane.showInputDialog(sTexto));
+            sTexto = "IDADE INVALIDA!!!\n\nDigite uma idade (Entre 0 e 150)";
             
         
-        }while(sNome.length()< 3);
+        }while(iIdade < 0 || iIdade > 150);
+        
+        sTexto = "Digite um salario (Maior que 0)";
+        
+        do{
+            
+            fSal = Float.parseFloat(JOptionPane.showInputDialog(sTexto));
+            sTexto = "SALARIO INVALIDO!!!\n\nDigite um salario (Maior que 0)";
+            
+        
+        }while(fSal<=0);
+        
+        sTexto = "Digite seu genero <M/F>";
+        
+        do{
+        
+            sGen = JOptionPane.showInputDialog(sTexto);
+            sTexto = "GENERO INVALIDO!!!\n\nOpcoes <M>, <F>";
+        
+        }while(!sGen.equalsIgnoreCase("M") && !sGen.equalsIgnoreCase("F"));
+        
+        JOptionPane.showMessageDialog(null, "Nome: "+sNome+"\nIdade: "+iIdade+"\nSalario: "+fSal+"\nGenero: "+sGen);
         
     }
     
