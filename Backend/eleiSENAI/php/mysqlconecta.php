9d387f8a76a285ba0bf4c991db006c7383702832
Miguel Caires Montalvão <133923664+MiguelCSenai@users.noreply.github.com>
2024-10-25 17:07:16 -0300

    Delete Backend/eleiSENAI directory

--- Backend/eleiSENAI/php/mysqlconecta.php
+++
@@ -1,9 +0,0 @@
-<?php
-$servidor = "127.0.0.1";
-$usuario = "root";
-$senha = "";
-$bancodados = "eleicao";
-
-$conexao = mysqli_connect($servidor, $usuario, $senha, $bancodados) or die("Problemas ao conectar ao banco!");
-
-?>
